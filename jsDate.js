c987bcdc117e837f43d49efb578af62fba290b18
Baseel-Bandak <bkbandak@gmail.com>
2018-08-06 16:52:23 +0200

    add color picker

--- jsDate.js
+++ jsDate.js
@@ -21,20 +21,30 @@ function setTime() {
     const second = now.getSeconds();
     const secondDegrees = ((second / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondDegrees}deg)`;
-    /*Digital Time*/secondDigital.innerHTML = `${second <10 ? '0':''}${second}`;
 
     const minute = now.getMinutes();
     const minuteDegrees = ((minute / 60) * 360) + 90;
     minuteHand.style.transform = `rotate(${minuteDegrees}deg)`;
-    /*Digital Time*/minuteDigital.innerHTML = `${minute <10 ? '0':''}${minute}`;;
 
     const hour = now.getHours();
     const hourDegrees = ((hour / 12) * 360) + 90;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-    /*Digital Time*/hourDigital.innerHTML = `${hour <10 ? '0':''}${hour}`;
 
-    /*Date*/ dayDigital.innerHTML =now.getDate();
-    /*Date*/ monthDigital.innerHTML =now.getMonth() +1;
-    /*Date*/ yearDigital.innerHTML =now.getYear() -100;
+    /*Digital Time*/secondDigital.innerHTML = `${second < 10 ? '0' : ''}${second}`;
+    /*Digital Time*/minuteDigital.innerHTML = `${minute < 10 ? '0' : ''}${minute}`;
+    /*Digital Time*/hourDigital.innerHTML = `${hour < 10 ? '0' : ''}${hour}`;
+
+    /*Date*/ dayDigital.innerHTML = now.getDate();
+    /*Date*/ monthDigital.innerHTML = now.getMonth() + 1;
+    /*Date*/ yearDigital.innerHTML = now.getYear() - 100;
 }
 setInterval(setTime, 1000);
+
+/*Change background color*/
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate() {
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`${this.name}`, this.value + suffix);
+}
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
